e6997094997915660e26a865e9cd1cdf929760f2
Roman <ivaniv.roma.15@gmail.com>
2023-01-16 23:05:49 +0200

    create spring project

---
+++ src/test/java/com/example/nix/NixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
